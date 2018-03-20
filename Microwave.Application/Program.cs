a6a6b5fd4cb4d2242fa5a0b7f8f6002d2a6a8038
Tobias <tobiasniklassen@gmail.com>
2018-03-20 18:53:58 +0100

    added sln

---
+++ Microwave.Application/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microwave.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Setup all the objects, 
+            //var door = new Door();
+            // ...
+            // etc.
+
+            // Simulate user activities
+            //powerButton.Press();
+            // ...
+            // etc. 
+
+
+
+            // Wait while the classes, including the timer, do their job
+            System.Console.WriteLine("Tast enter når applikationen skal afsluttes");
+            System.Console.ReadLine();
+        }
+    }
+}
